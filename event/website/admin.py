b9eefe935faa17ac75950c66361a612fd0655766
Icarus-afk <ahmed.ehsan1258@gmail.com>
2023-03-10 18:53:48 +0600

    database

--- event/website/admin.py
+++ event/website/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Family, Business, Charity, Culture, Book_event, Venue, Contact_us
 
-# Register your models here.
+admin.site.register(Family)
+admin.site.register(Business)
+admin.site.register(Charity)
+admin.site.register(Culture)
+admin.site.register(Venue)
+admin.site.register(Book_event)
+admin.site.register(Contact_us)
--- event/website/models.py
+++ event/website/models.py
@@ -1,3 +1,104 @@
-from django.db import models
+from django.db import models 
+from django.contrib.auth.models import User
 
-# Create your models here.
+#Family
+
+class Family(models.Model):
+    event_type = models.CharField(max_length = 300)
+    desc = models.TextField()
+    image = models.ImageField(upload_to = "family_images")
+    food = models.CharField(max_length = 200)
+    decoration = models.CharField(max_length = 500)
+    user = models.ForeignKey(User, on_delete = models.CASCADE, default = " ")
+
+    def __str__(self):
+        return self.name
+    
+    
+ #Charity 
+    
+class Charity(models.Model):
+    event_type = models.CharField(max_length = 300)
+    desc = models.TextField()
+    image = models.ImageField(upload_to = "charity_images")
+    food = models.CharField(max_length = 200)
+    decoration = models.CharField(max_length = 500)
+    chief_guest = models.CharField(max_length = 100)
+    sponsor = models.CharField(max_length = 100)
+    user = models.ForeignKey(User, on_delete = models.CASCADE, default = " ")
+
+    def __str__(self):
+        return self.name
+    
+    
+#Business
+
+class Business(models.Model):    
+    event_type = models.CharField(max_length = 300)
+    desc = models.TextField()
+    image = models.ImageField(upload_to = 'business_image')
+    food = models.CharField(max_length = 200)
+    chief_guest = models.CharField(max_length = 100)
+    user = models.ForeignKey(User, on_delete = models.CASCADE, default = " ")
+    
+    def __str__(self):
+        return self.name
+    
+    
+#Culture
+
+class Culture(models.Model):
+    event_type = models.CharField(max_length = 300)
+    desc = models.TextField()
+    image = models.ImageField(upload_to = 'culture_image')
+    food = models.CharField(max_length = 200)
+    chief_guest = models.CharField(max_length = 100)
+    user = models.ForeignKey(User, on_delete = models.CASCADE, default = " ")
+    
+    def __str__(self):
+        return self.name
+    
+    
+    
+class Venue(models.Model):
+    name = models.CharField(max_length = 50)
+    address = models.CharField(max_length = 500)
+    capacity = models.IntegerField()
+    desc = models.TextField()
+    image = models.ImageField(upload_to = "venue")
+    
+    def __str__(self):
+        return self.name
+    
+#Book Event
+
+class Book_event(models.Model):
+    name = models.CharField(max_length = 100)
+    mobile = models.CharField(max_length = 14)
+    email = models.EmailField()
+    location = models.CharField(max_length = 100)
+    people = models.IntegerField()
+    date = models.DateField()
+    event = models.CharField(max_length = 100)
+    food = models.CharField(max_length = 100)
+    address = models.CharField(max_length = 500)
+    venue = models.ForeignKey(Venue, on_delete = models.CASCADE, default = " ")
+    message = models.TextField()
+    
+    def __str__(self):
+        return self.name
+        
+#Contact us
+
+class Contact_us(models.Model):
+    name = models.CharField(max_length = 50)
+    mobile = models.CharField(max_length = 14)
+    email = models.EmailField()
+    message = models.TextField()
+    
+    def __str__(self):
+        return self.name 
+    
+    
+    
+    
